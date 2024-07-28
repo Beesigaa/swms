795f63e4aaca80de0c1958f1395218b4d6b17478
Adejoh <113752830+Beesigaa@users.noreply.github.com>
2024-07-28 20:39:24 +0100

    Delete backend/waste_management_system directory

--- backend/waste_management_system/bins/serializers.py
+++
@@ -1,8 +0,0 @@
-# bins/serializers.py
-from rest_framework import serializers
-from .models import Bin
-
-class BinSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Bin
-        fields = '__all__'
